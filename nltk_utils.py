b490aae409e88b7ef19075a81d00ffb32e6de1fd
Juan Aragon <juanm.aramas@gmail.com>
2022-03-01 23:46:10 +0100

    model trained

--- nltk_utils.py
+++ nltk_utils.py
@@ -17,6 +17,11 @@ def bag_of_words(tokenized_sentence, all_words):
     
     tokenized_sentence = [stem(w) for w in tokenized_sentence]
     bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    
+    return bag
 
      
 
@@ -33,3 +38,9 @@ if __name__ == '__main__':
     print(words)
     print(stemmed_words)
     print('-------------------------')
+    
+    sentence = ['hello', 'how','are', 'you']
+    words = ['hi', 'hello', 'I','you', 'bye', 'thank', 'cool']
+    bag  = bag_of_words(sentence, words)
+    print(bag)
+
