3a5712fc1151aa713640ae15538badb78ccb00ca
Juan Aragon <juanm.aramas@gmail.com>
2022-03-02 08:58:07 +0100

    added frontend in js

--- chat.py
+++ chat.py
@@ -5,32 +5,7 @@ from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-with open('intents.json', 'r') as f:
-    intents = json.load(f)
-
-FILE = 'data.pth'
-data = torch.load(FILE)
-input_size = data['input_size']
-hidden_size = data['hidden_size']
-num_classes = data['num_classes']
-tags = data['tags']
-all_words = data['all_words']
-
-model_state = data['model_state']
-
-model = NeuralNet(input_size, hidden_size, num_classes).to(device) 
-model.load_state_dict(model_state)
-model.eval()
-
-bot_name = 'Sam'
-print("Let's chat! type 'quit' to exit")
-while True:
-    sentence = input('You: ')
-    if sentence == 'quit':
-        break
-    
+def get_response(sentence):
     sentence = tokenize(sentence)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
@@ -51,3 +26,34 @@ while True:
     
     else:
         print(f"{bot_name}: I do not understand...")
+
+if __name__ == '__main__':
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    with open('intents.json', 'r') as f:
+        intents = json.load(f)
+    
+    FILE = 'data.pth'
+    data = torch.load(FILE)
+    input_size = data['input_size']
+    hidden_size = data['hidden_size']
+    num_classes = data['num_classes']
+    tags = data['tags']
+    all_words = data['all_words']
+    
+    model_state = data['model_state']
+    
+    model = NeuralNet(input_size, hidden_size, num_classes).to(device) 
+    model.load_state_dict(model_state)
+    model.eval()
+    
+    bot_name = 'Sam'
+    
+    print("Let's chat! type 'quit' to exit")
+    while True:
+        sentence = input('You: ')
+        if sentence == 'quit':
+            break
+        get_response(sentence)
+        
